3f8fe993b6f19d60e84ca45881dae79e3d13089e
Gaurav Daware <gauravdaware02@gmail.com>
2025-02-22 00:50:35 +0530

    2nd commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -66,7 +70,13 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier.fillMaxSize()) {
         Image(painter = painterResource(image),
             contentDescription = "$result")
         Spacer(modifier= Modifier.height(5.dp))
-        Button(onClick = {result = (1..6).random()}) { Text("Roll") }
+        Button(onClick = {result = (1..6).random()},
+            colors= ButtonDefaults.buttonColors(Color(0xFFab81b6))){
+            Text(text = "Roll",
+                fontSize = 18.sp,
+                modifier = Modifier.padding(horizontal = 8.dp))
+
+            }
     }
 
     
